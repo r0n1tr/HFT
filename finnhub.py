1981c258918cbdd5b46911c80dd9614025b496e0
Ronit <138166258+r0n1tr@users.noreply.github.com>
2024-07-13 14:21:42 +0100

    format edits

--- finnhub.py
+++ finnhub.py
@@ -9,10 +9,10 @@ while True:
     url = f'https://finnhub.io/api/v1/quote?symbol={symbol}&token={API_KEY}'
     response = requests.get(url)
     data = response.json()
-
+    
     latest_price = data['c']  # Current price
     latest_time = data['t']  # Unix timestamp
-    latest_time_formatted = datetime.fromtimestamp(latest_time, tz=timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
-    print(f"Time: {latest_time}, Latest Price: {latest_time_formatted}")
+    latest_time_formatted = datetime.fromtimestamp(latest_time, tz=timezone.utc).strftime('%H:%M:%S %d-%m-%Y')
+    print(f"{symbol} - Time: {latest_time}, Latest Price: {latest_time_formatted}")
 
     time.sleep(1)
