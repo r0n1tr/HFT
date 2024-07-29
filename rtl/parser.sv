37ea6c7c29e48efbcc99d9501f787d8577ae5831
sne-samal <sne.samal@gmail.com>
2024-07-29 12:19:12 +0100

    parser valid and busy signal

--- rtl/parser.sv
+++ rtl/parser.sv
@@ -15,6 +15,7 @@ module parser
 )
 (
     input logic                         i_clk, 
+    input logic                         i_book_is_busy,
     input logic [REG_WIDTH - 1 : 0]     i_reg_1,
     input logic [REG_WIDTH - 1 : 0]     i_reg_2,
     input logic [REG_WIDTH - 1 : 0]     i_reg_3,
@@ -27,7 +28,8 @@ module parser
     output logic [REG_WIDTH - 1 : 0]    o_price,
     output logic [15:0]                 o_quantity,
     output logic [1:0]                  o_order_type,
-    output logic                        o_trade_type
+    output logic                        o_trade_type,
+    output logic                        o_valid
 );
 
     typedef enum logic [1:0] { 
@@ -58,40 +60,46 @@ module parser
 
     always_ff @(posedge i_clk) begin
 
-        case(i_reg_1[31:24])
-            8'h41: begin
-                o_order_type <= ADD;
-                o_stock_symbol <= stock_id;
-                o_order_id <= {i_reg_2[23:0], i_reg_3[31:24]};
-                o_price <= {i_reg_6[15:0], i_reg_7[31:16]};
-                o_quantity <= i_reg_4[31:16];
-                o_trade_type <= i_reg_3[16] ? BUY : SELL;
-            end
-            8'h58: begin
-                o_order_type <= CANCEL;
-                o_stock_symbol <= 0;
-                o_order_id <= {i_reg_2[23:0], i_reg_3[31:24]};
-                o_price <= 0;
-                o_quantity <= 0;
-                o_trade_type <= 0;
-            end
-            8'h45: begin 
-                o_order_type <= EXECUTE;
-                o_stock_symbol <= 0;
-                o_order_id <= {i_reg_2[23:0], i_reg_3[31:24]};
-                o_price <= 0;
-                o_quantity <= {i_reg_3[7:0], i_reg_4[31:24]};
-                o_trade_type <= 0;
-            end
-            default: begin 
-                o_order_type <= 0;
-                o_stock_symbol <= 0;
-                o_order_id <= 0;
-                o_price <= 0;
-                o_quantity <= 0;
-                o_trade_type <= 0;
-            end
-        endcase
+        if(!i_book_is_busy) begin
+            o_valid <= 1;
+            case(i_reg_1[31:24])
+                8'h41: begin
+                    o_order_type <= ADD;
+                    o_stock_symbol <= stock_id;
+                    o_order_id <= {i_reg_2[23:0], i_reg_3[31:24]};
+                    o_price <= {i_reg_6[15:0], i_reg_7[31:16]};
+                    o_quantity <= i_reg_4[31:16];
+                    o_trade_type <= i_reg_3[16] ? BUY : SELL;
+                end
+                8'h58: begin
+                    o_order_type <= CANCEL;
+                    o_stock_symbol <= 0;
+                    o_order_id <= {i_reg_2[23:0], i_reg_3[31:24]};
+                    o_price <= 0;
+                    o_quantity <= 0;
+                    o_trade_type <= 0;
+                end
+                8'h45: begin 
+                    o_order_type <= EXECUTE;
+                    o_stock_symbol <= 0;
+                    o_order_id <= {i_reg_2[23:0], i_reg_3[31:24]};
+                    o_price <= 0;
+                    o_quantity <= {i_reg_3[7:0], i_reg_4[31:24]};
+                    o_trade_type <= 0;
+                end
+                default: begin 
+                    o_order_type <= 0;
+                    o_stock_symbol <= 0;
+                    o_order_id <= 0;
+                    o_price <= 0;
+                    o_quantity <= 0;
+                    o_trade_type <= 0;
+                end
+            endcase
+        end
+        else begin
+            o_valid <= 0;
+        end
     end
 
     always_comb begin
