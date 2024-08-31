04dea13a2a8e391babb57f3ae1ebdfebedf00ac2
sne-samal <sne.samal@gmail.com>
2024-08-31 10:46:53 +0100

    potential bug with inserting orders

--- model/test.py
+++ model/test.py
@@ -10,7 +10,7 @@ my_model = MarketMakingModel()
 #     # print(len(my_exchange.generate_ITCH_order(num, hex_output=True, printing=True)))
 #     num = random.randint(0,3)
 #     (my_exchange.generate_ITCH_order(num, integer_output=True, printing=True))
-for _ in range (100):
+for _ in range (8000):
     num = random.randint(0,3)
     input_vector = my_exchange.generate_ITCH_order(num, printing=True, integer_output=True)
     # print(my_exchange.generate_ITCH_order(num, printing=False, integer_output=False))
@@ -22,6 +22,6 @@ for _ in range (100):
     # print(f"Exchange: {input_vector}")
     print(f"Market Maker Sell Order: {sell_order}")
     print(f"Market Maker Buy Order: {buy_order}")
-    print("Orders Inserted")
+    # print("Orders Inserted")
     my_exchange.insert_into_exchange(order_information_buy)
     my_exchange.insert_into_exchange(order_information_sell)
