cd6f573ad51e11850a7552c248866eded6e066ce
r0n1tr <rr422@ic.ac.uk>
2024-09-01 12:40:06 +0100

    updated testfile, will need to change quote_order calling parser for it to work

--- model/test.py
+++ model/test.py
@@ -1,4 +1,4 @@
-from exchange import Exchange
+from exchange import Exchange, generate_timestamp
 from market_maker import MarketMakingModel
 import random
 
@@ -13,15 +13,29 @@ my_model = MarketMakingModel()
 for _ in range (100):
     num = random.randint(0,3)
     input_vector = my_exchange.generate_ITCH_order(num, printing=True, integer_output=True)
+    print(len(input_vector))
     # print(my_exchange.generate_ITCH_order(num, printing=False, integer_output=False))
     sell_order, buy_order = my_model.quote_orders(input_vector)
-    order_information_buy = buy_order
-    order_information_buy.append(1)
-    order_information_sell = sell_order
-    order_information_sell.append(0)
     # print(f"Exchange: {input_vector}")
     print(f"Market Maker Sell Order: {sell_order}")
     print(f"Market Maker Buy Order: {buy_order}")
+    
+    temp_vector_b = my_exchange.insert_into_exchange(buy_order)
+    temp_vector_s = my_exchange.insert_into_exchange(sell_order)
     print("Orders Inserted")
-    my_exchange.insert_into_exchange(order_information_buy)
-    my_exchange.insert_into_exchange(order_information_sell)
+    print(temp_vector_b)
+    # quote_order require -> stock_id, order_id, trade_type, quantity, price, order_type, timestamp
+
+    sell_order, buy_order = my_model.quote_orders(temp_vector_b)
+    print(f"Market Maker Sell Order: {sell_order}")
+    print(f"Market Maker Buy Order: {buy_order}")
+    sell_order, buy_order = my_model.quote_orders(temp_vector_s)
+    print(f"Market Maker Sell Order: {sell_order}")
+    print(f"Market Maker Buy Order: {buy_order}")
+
+    print("Cycle Complete. \n")
+    # generate itch for own order produces stock_id, unique_id, order_quantity, price
+    # so we are missing order_type, timestamp, order_side, 
+
+    # normal gen itch produces order_type, timestamp, order_id, order_side, quantity, stock_id, price
+    
